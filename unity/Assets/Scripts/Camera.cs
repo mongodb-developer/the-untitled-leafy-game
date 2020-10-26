9c1901907dae25180116d5a1d7535df241197599
Nic Raboy <nicolas.raboy@mongodb.com>
2020-10-26 08:33:12 -0700

    level one boilerplate

---
+++ unity/Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+}
---
+++ unity/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rigidbody;
+    private bool isGrounded;
+
+    [Range(1, 5)]
+    public float speed;
+
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    [Range(1, 5)]
+    public float fallingMultiplier;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+    }
+
+    void Update() { }
+
+    void FixedUpdate() {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        if(Input.GetKey(KeyCode.Space) && isGrounded == true) {
+            rigidbody.velocity += Vector2.up * jumpVelocity;
+            isGrounded = false;
+        }
+
+        if (rigidbody.velocity.y < 0) {
+            rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallingMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (rigidbody.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
+            rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallingMultiplier - 1) * Time.fixedDeltaTime;
+        }
+
+        rigidbody.velocity = new Vector2(horizontalMovement * speed, rigidbody.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.name == "Ground" || collision.collider.name == "Platforms") {
+            isGrounded = true;
+        }
+    }
+
+}
