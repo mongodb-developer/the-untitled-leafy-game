4ead880d577539d3d3698d3f816fb8b4f8a60441
ahhere <barry.oneill@mongodb.com>
2020-10-27 15:50:46 -0400

    pushing

---
+++ unity/Assets/Scripts/PopUpUi.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PopUpUi : MonoBehaviour
+{
+public GameObject QuestionBox;
+
+
+// Use this for initialization
+void Start()
+{
+    QuestionBox.SetActive(false);
+
+}
+
+
+// Update is called once per frame
+void Update()
+{
+
+}
+
+void OnCollisionEnter2D(Collision2D collision)
+{
+QuestionBox.SetActive(true);
+
+}
+
+}
