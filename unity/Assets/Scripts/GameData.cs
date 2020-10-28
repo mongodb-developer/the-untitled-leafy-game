acd85f13db70773e484af0f1ec8be9f2b1a1fad0
Nic Raboy <nicolas.raboy@mongodb.com>
2020-10-28 07:31:07 -0700

    get score to persist between levels

---
+++ unity/Assets/Scripts/GameData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameData
+{
+
+    public static int totalScore;
+
+    // public static void setScore(int score) {
+    //     this.score = score;
+    // }
+
+    // public static int getScore() {
+    //     return this.score;
+    // }
+
+}
--- unity/Assets/Scripts/Score.cs
+++ unity/Assets/Scripts/Score.cs
@@ -9,27 +9,26 @@ public class Score : MonoBehaviour
     private Text scoreText;
     private int score;
 
-    // Start is called before the first frame update
     void Start()
     {
         scoreText = GetComponent<Text>();
-        score = 0;
+        this.Reset();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     public void Reset() {
-        score = 0;
-        scoreText.text = "SCORE: " + score;
+        score = GameData.totalScore;
+        scoreText.text = "SCORE: " + GameData.totalScore;
     }
 
     public void AddPoints(int amount) {
         score += amount;
         scoreText.text = "SCORE: " + score;
+    }
+
+    public void BankScore() {
+        GameData.totalScore += score;
     }
 
     void PersistScore() {
